247091839a201c633ee363890aa1009be8a57c86
Lenora Stevens <lenoraRStevens@gmail.com>
2024-09-18 21:23:06 -0500

    work on contacts project,  connecting to MongoDB

---
+++ Contacts-Project/data/contacts.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv').config();
+
+const MongoClient = require('mongodb').MongoClient;
+
+let contactsData;
+
+const initDb = (callback) => {
+    if (contactsData) {
+        console.log('Db is already initialized!');
+        return callback(null, contactsData)
+    }
+    MongoClient.connect(process.env.MONGODB_URL)
+        .then((client) => {
+            contactsData = client;
+            callback(null, contactsData);
+        })
+        .catch((err) => {
+            callback(err);
+        });
+};
+
+const getContactsData = () => {
+    if (!contactsData) {
+        throw Error('Contacts Data not initialized')
+    }
+    return contactsData;
+};
+
+module.exports = {
+    initDb,
+    getContactsData
+};
--- Contacts-Project/server.js
+++ Contacts-Project/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongodb = require('./data/contacts');
 const app = express();
 
 const port = 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(process.env.PORT || port, () => {
-    console.log('Web Server is listening at port ' + (process.env.PORT || port));
-  });
+mongodb.initDb((err) => {
+  if(err) {
+    console.log(err);
+  }
+  else {
+    app.listen(process.env.PORT || port, () => {
+      console.log('Database is listening at port ' + (process.env.PORT || port));
+    });
+  }
+});
+
