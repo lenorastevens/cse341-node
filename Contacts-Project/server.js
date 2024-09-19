692fb962d261e48246c78abcf059ea9fbd9c8bca
Lenora Stevens <lenoraRStevens@gmail.com>
2024-09-18 22:06:05 -0500

    port change

--- Contacts-Project/server.js
+++ Contacts-Project/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongodb = require('./data/contacts');
 const app = express();
 
-const port = 3000;
+const port = 8080;
 
 app.use('/', require('./routes'));
 
