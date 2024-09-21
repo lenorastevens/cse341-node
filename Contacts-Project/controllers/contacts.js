acfc43cd81c52a40efc16783e78948c91fa91204
Lenora Stevens <lenoraRStevens@gmail.com>
2024-09-20 22:11:30 -0500

    correcting database errors

--- Contacts-Project/controllers/contacts.js
+++ Contacts-Project/controllers/contacts.js
@@ -1,5 +1,5 @@
 const mongodb = require('../data/contactsDb');
-const ObjectId = require('mongodb').ObjectId;
+const { ObjectId } = require('mongodb');
 
 const getAll = async (req, res) => {
     const result = await mongodb.getContactsData().db().collection('contacts').find();
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getContactsData().db().collection('contacts').find({ _id: userId });
+    const result = await mongodb.getContactsData().db().collection('contacts').findOne({ _id: userId });
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
