f16787ce9c6dc089f41f7d5d53dae0f4d01b7c71
Lenora Stevens <lenoraRStevens@gmail.com>
2024-09-26 13:40:29 -0500

    fine tuning

--- Contacts-Project/routes/index.js
+++ Contacts-Project/routes/index.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send("Hello, CSE 341. Documentation URL: https://cse341-contacts-project-ep1p.onrender.com");
-  });
+// router.get('/', (req, res) => {
+//     res.send("Hello, CSE 341. Documentation URL: https://cse341-contacts-project-ep1p.onrender.com");
+//   });
 
 router.use('/contacts', require('./contacts'));
+
+routes.use('/', (docData = (req, res) => {
+  let docData = {
+    documentationURL: 'https://cse341-contacts-project-ep1p.onrender.com/api-docs',
+  };
+  res.send(docData);
+})
+);
 
 module.exports = router;
