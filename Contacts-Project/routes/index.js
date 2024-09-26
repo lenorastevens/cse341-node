c5b5a5af34e3d7ac388f18ddb6c4308a8475cd02
Lenora Stevens <lenoraRStevens@gmail.com>
2024-09-26 18:22:14 -0500

    fixing landing page look and link

--- Contacts-Project/routes/index.js
+++ Contacts-Project/routes/index.js
@@ -6,10 +6,13 @@ const router = require('express').Router();
 
 router.use('/contacts', require('./contacts'));
 
-router.use('/', (docData = (req, res) => {
-  let docData = "documentationURL: https://cse341-contacts-project-ep1p.onrender.com/api-docs";
+router.use('/', (req, res) => {
+  let docData = `
+    <h1>Hello, Fall CSE 341. This is my Contacts Project!<h1>
+    <p>Documentation URL: <a href="https://cse341-contacts-project-ep1p.onrender.com/api-docs" target="_blank">
+    https://cse341-contacts-project-ep1p.onrender.com/api-docs</a></p>
+  `;
   res.send(docData);
-})
-);
+});
 
 module.exports = router;
