253cf82cef88326d27bcecd82f6ce7eee84c3e35
Lenora Stevens <lenoraRStevens@gmail.com>
2024-09-26 18:23:59 -0500

    fixed h1

--- Contacts-Project/routes/index.js
+++ Contacts-Project/routes/index.js
@@ -8,7 +8,7 @@ router.use('/contacts', require('./contacts'));
 
 router.use('/', (req, res) => {
   let docData = `
-    <h1>Hello, Fall CSE 341. This is my Contacts Project!<h1>
+    <h1>Hello, Fall CSE 341. This is my Contacts Project!</h1>
     <p>Documentation URL: <a href="https://cse341-contacts-project-ep1p.onrender.com/api-docs" target="_blank">
     https://cse341-contacts-project-ep1p.onrender.com/api-docs</a></p>
   `;
