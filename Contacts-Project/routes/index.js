5e2e4daa85e2daecad37dd0cc69ca4f23513b09e
Lenora Stevens <lenoraRStevens@gmail.com>
2024-10-10 21:37:29 -0500

    adjusting github callback

--- Contacts-Project/routes/index.js
+++ Contacts-Project/routes/index.js
@@ -1,23 +1,7 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-router.get('/login', passport.authenticate('github'));
-
-router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),  
-  (req, res) => {
-    console.log('Callback route hit');
-    console.log('Authenticated user:', req.user);
-    req.session.user = req.user;
-    res.redirect('/');
-});
-
-router.get('/profile', (req, res) => {
-  if (req.session.user) {
-    res.send(`Logged in as ${req.session.user.displayName}`);
-  } else{
-    res.send('Not Logged In');
-  }
-});
+router.get('/login', passport.authenticate('github'), (req, res) => {});
 
 router.get('/logout', (req, res) => {
   req.logout((err) => {
--- Contacts-Project/server.js
+++ Contacts-Project/server.js
@@ -50,6 +50,24 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out')});
+
+app.get('/github/callback', passport.authenticate('github', { failureRedirect: '/api-docs' }),  
+  (req, res) => {
+    console.log('Callback route hit');
+    console.log('Authenticated user:', req.user);
+    req.session.user = req.user;
+    res.redirect('/');
+});
+
+// router.get('/profile', (req, res) => {
+//   if (req.session.user) {
+//     res.send(`Logged in as ${req.session.user.displayName}`);
+//   } else{
+//     res.send('Not Logged In');
+//   }
+// });
+
 mongodb.initDb((err) => {
   if(err) {
     console.log(err);
