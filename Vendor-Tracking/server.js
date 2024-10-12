28654f7bffd7353aa67bc1f4994ea8408c64a2c0
Lenora Stevens <lenoraRStevens@gmail.com>
2024-10-11 23:34:30 -0500

    uncommented app.get route

--- Vendor-Tracking/server.js
+++ Vendor-Tracking/server.js
@@ -41,7 +41,7 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-// app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out')});
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out')});
 
 app.get('/github/callback', passport.authenticate('github', { failureRedirect: '/api-docs' }),  
   (req, res) => {
