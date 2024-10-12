7c22f3a99a6b768cba6496a99b9a4f94a2d35547
Lenora Stevens <lenoraRStevens@gmail.com>
2024-10-11 22:48:26 -0500

    changing home screen view

--- Vendor-Tracking/routes/index.js
+++ Vendor-Tracking/routes/index.js
@@ -16,13 +16,13 @@ router.use('/vendors', require('./vendors'));
 
 router.use('/appUsers', require('./appUsers'));
 
-router.use('/', (req, res) => {
-    let docData = `
-      <h1>Vendor Tracking API</h1>
-      <p>Documentation URL: <a href="https://vendor-tracking.onrender.com/api-docs" target="_blank">
-      https://vendor-tracking.onrender.com/api-docs</a></p>
-    `;
-    res.send(docData);
-  });
+// router.use('/', (req, res) => {
+//     let docData = `
+//       <h1>Vendor Tracking API</h1>
+//       <p>Documentation URL: <a href="https://vendor-tracking.onrender.com/api-docs" target="_blank">
+//       https://vendor-tracking.onrender.com/api-docs</a></p>
+//     `;
+//     res.send(docData);
+//   });
 
 module.exports = router;
